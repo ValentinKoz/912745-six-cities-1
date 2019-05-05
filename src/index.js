bc8e2c88b1ee319756835e4d20328845b9aa0d36
Valentin Kozhin <kozhin_val@mail.ru>
2019-05-05 23:18:36 +0300

    Личный проект: валидируй, как никогда не валидировал

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const init = () => {
+  const dataForRent = {
+    names: [`Beautiful & luxurious apartment at great location`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`, `Wood and stone place`],
+  };
+
   ReactDOM.render(
-      <App/>,
+      <App
+        names={dataForRent.names}
+      />,
       document.getElementById(`root`)
   );
 };
